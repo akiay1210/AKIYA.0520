eebb1113b16f42119975468d662829af797055aa
akiay1210 <zm24374@josai.ac.jp>
2025-11-11 09:30:14 +0900

    Remove duplicate function definitions

--- ex6-7.js
+++ ex6-7.js
@@ -1,4 +1,2 @@
 var who67 = 'world'; function sayhello67() { alert( 'Hello, ' + who67 + '!'); }
 function someoen(太郎) { who67 = 太郎; }
-var who67 = 'world'; function sayhello67() { alert( 'Hello, ' + who67 + '!'); }
-function someoen(花子) { who67 = 花子; }
