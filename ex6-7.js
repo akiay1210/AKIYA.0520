af85ac9da094d8e868d1c74ed64e8fe0f90dd661
akiay1210 <zm24374@josai.ac.jp>
2025-11-11 09:29:11 +0900

    Fix variable declaration and function definition

--- ex6-7.js
+++ ex6-7.js
@@ -1,4 +1,4 @@
 var who67 = 'world'; function sayhello67() { alert( 'Hello, ' + who67 + '!'); }
 function someoen(太郎) { who67 = 太郎; }
-ar who67 = 'world'; function sayhello67() { alert( 'Hello, ' + who67 + '!'); }
+var who67 = 'world'; function sayhello67() { alert( 'Hello, ' + who67 + '!'); }
 function someoen(花子) { who67 = 花子; }
