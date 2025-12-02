ef35e2b871eafa497d6e9b855655616d35961ad9
akiay1210 <zm24374@josai.ac.jp>
2025-12-02 09:42:09 +0900

    Add sayhello9 function to update innerHTML

---
+++ ex6-9.js
@@ -0,0 +1,3 @@
+function sayhello9() { alert('Hello,' + document.getElementByld('who69).innerHTML +'!'); }
+function sayhello9() { document.getElementByld('who69').innerHTML = '太郎'; }
+function sayhello9() { document.getElementByld('who69').innerHTML = '花子'; }                                                              
