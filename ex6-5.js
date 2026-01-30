aec1da63ccf11c1bcb72664fd5e5cc4b2f52090a
akiay1210 <zm24374@josai.ac.jp>
2026-01-30 23:23:36 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,4 +1,7 @@
-var who65 = 'world'; function sayhello65() { alert('Hello, '+ who65 + '!'); }
-function taro65() { who65 ='太郎'; }
-function hanako65() { who65 = '花子'; }
-function come65() { alert( who65 + 'が来た！'); }
+var who65= 'world';
+function sayhello61(){
+   alert('Hello, Function!');
+}
+function saygoodbye61(){
+   alert('Goodbye.');
+}
